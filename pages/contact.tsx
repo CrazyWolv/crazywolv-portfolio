387435c9da719e443ab04dc188d3a969a061ac12
CrazyWolv <59683917+CrazyWolv@users.noreply.github.com>
2022-04-07 19:18:28 +0200

    Imports

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
-import Contact from '@components/Contact.js'
-import Layout from '@components/Layout.js'
+import Contact from '@/components/Contact.js'
+import Layout from '@/components/Layout.js'
 
 export default function ContactMe() {
   return (
