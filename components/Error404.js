221a3555bda8e949b87025f27574b6af13ceab50
CrazyWolv <59683917+CrazyWolv@users.noreply.github.com>
2022-04-15 15:28:39 +0200

    Added Link import to Custom 404 page

--- components/Error404.js
+++ components/Error404.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Footer from "./footer";
+import Link from 'next/link';
 
 export default function Error404() {
     const meta = {
@@ -22,7 +23,7 @@ export default function Error404() {
                 <div className="flex flex-col-reverse items-center w-full mx-auto overflow-hidden py-10 lg:flex-row lg:justify-around">
                     <div className="w-full lg:w-1/2 bg-white dark:bg-dark flex flex-col items-center gap-y-3 px-6">
                         <h1 className="text-4xl lg:text-6xl font-bold text-lightThemeText dark:text-darkThemeText text-center underlined underlined-green underlined-bold hover:underlined-hover">Une 404 sauvage apparaît !</h1>
-                        <p className="w-full md:w-3/4 px-3 py-2 text-xl leading-loose">Il semblerait que vous ayez perdu votre chemin ! Lorsque vous aurez fini d'observer cet adorable boule de poils, vous pourrez cliquer <a href="/" className="errorLink hover:errorLinkHover">ici</a> ou même encore <a href="/" className="errorLink hover:errorLinkHover">là</a> pour <span className="font-bold">revenir à l'accueil</span>.</p>
+                        <p className="w-full md:w-3/4 px-3 py-2 text-xl leading-loose">Il semblerait que vous ayez perdu votre chemin ! Lorsque vous aurez fini d'observer cet adorable boule de poils, vous pourrez cliquer <Link href="/"><a className="errorLink hover:errorLinkHover">ici</a></Link> ou même encore <Link href="/"><a className="errorLink hover:errorLinkHover">là</a></Link> pour <span className="font-bold">revenir à l'accueil</span>.</p>
                     </div>
                     <div className="w-full lg:w-1/2 lg:mr-5">
                         <img src="/img/error404.png" className="block" alt="Error 404 page not found" />
